0a882eec97cde3d0084721545285b1167743b4a4
Kashish Khera <kashishkhera20@gmail.com>
2025-05-18 23:39:50 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,50 @@
+# Few library Imported
+
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+@dataclass
+class DataIngestionCongig:
+    test_data_path: str = os.path.join('artifacts',"test.csv")
+    train_data_path: str = os.path.join('artifacts',"train.csv")
+    raw_data_path: str = os.path.join('artifacts',"data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestionConfig = DataIngestionCongig()
+
+    def initiatedataIngestion(self):
+        try:
+            df = pd.read_csv('StudentsPerformance.csv')
+            logging.info('File has been read')
+            os.makedirs(os.path.dirname(self.ingestionConfig.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestionConfig.raw_data_path,index=False,header=True)
+
+            logging.info('Train Test Spilit Initated')
+
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestionConfig.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestionConfig.test_data_path,index=False,header= True)
+
+            logging.info('Train Test Split Completed')
+
+            return(
+                self.ingestionConfig.train_data_path,
+                self.ingestionConfig.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiatedataIngestion()
